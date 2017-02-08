65dfbec68b67466ae546f108b2292c62d708e5ec
Alejandro Morente <morente5@hotmail.es>
2017-02-08 17:34:58 +0100

    Imported Collapse and Dropdown ng2-bootstrap modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// ng2-bootstrap modules
+import { CollapseModule } from 'ng2-bootstrap';
+import { DropdownModule } from 'ng2-bootstrap';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
@@ -16,7 +20,11 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    // ng2-bootstrap modules
+    CollapseModule.forRoot(),
+    DropdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
