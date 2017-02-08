4b93274b7e685a036cf9d92162bcd7f14a54d6ab
Alejandro Morente <morente5@hotmail.es>
2017-02-08 13:49:14 +0100

    Generated navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+	// Collapsed by default
+	public isCollapsed:boolean = true;
+
+
+	public collapsed(event:any):void {
+	  console.log(event);
+	}
+	
+	public expanded(event:any):void {
+	  console.log(event);
+	}
+
+	public toggled(open:boolean):void {
+	  console.log('Dropdown is now: ', open);
+	}
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
